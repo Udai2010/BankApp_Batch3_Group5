7334b3b9de368d9e20759e05f270aa85871131d2
Kris.Dsz <kris.dsouza2001@gmail.com>
2023-09-11 20:07:36 +0530

    Login FE BE Integration

--- bankapp/src/main/java/com/bankapp/bankapp/controller/CustomerController.java
+++ bankapp/src/main/java/com/bankapp/bankapp/controller/CustomerController.java
@@ -1,15 +1,20 @@
 package com.bankapp.bankapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bankapp.bankapp.model.Customer;
+import com.bankapp.bankapp.model.Login;
 import com.bankapp.bankapp.service.CustomerService;
 
 @RestController
+@CrossOrigin("*")
 public class CustomerController {
 	
 	@Autowired
@@ -24,6 +29,12 @@ public class CustomerController {
 	public Customer createNewCustomer(@RequestBody Customer c) {
 		Customer obj = customerService.createNewCustomer(c);
 		return obj;
+		
+	}
+	
+	@PostMapping("/login")
+	public String validateUser(@Valid @RequestBody Login l) {
+		return customerService.validateUser(l);
 		
 	}
 
