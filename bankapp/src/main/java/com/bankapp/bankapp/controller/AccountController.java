6f248cf0b027fd7dc2aa74c86956a89b2fd736f0
Kris.Dsz <kris.dsouza2001@gmail.com>
2023-09-13 11:26:16 +0530

    Account BE

--- bankapp/src/main/java/com/bankapp/bankapp/controller/AccountController.java
+++ bankapp/src/main/java/com/bankapp/bankapp/controller/AccountController.java
@@ -1,7 +1,5 @@
 package com.bankapp.bankapp.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bankapp.bankapp.model.Account;
-import com.bankapp.bankapp.model.Customer;
 import com.bankapp.bankapp.repository.CustomerRepository;
 import com.bankapp.bankapp.service.AccountService;
 
