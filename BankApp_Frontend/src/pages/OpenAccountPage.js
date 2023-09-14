47efa7c2bd58ce1f018b5987df48029f10b40ac5
ParthS28 <parthshukla285@gmail.com>
2023-09-14 08:21:18 +0530

    Added styling to open account

--- BankApp_Frontend/src/pages/OpenAccountPage.js
+++ BankApp_Frontend/src/pages/OpenAccountPage.js
@@ -2,6 +2,34 @@ import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Checkbox, FormControlLabel, Grid } from '@mui/material';
+import Backdrop from '@mui/material/Backdrop';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import { useNavigate } from 'react-router-dom';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const defaultTheme = createTheme();
+
 export default function OpenAccountPage() {
   const baseURL = "http://localhost:3000/createAccount/";
   const [customerId,setCustomerId]=useState("");
@@ -11,6 +39,17 @@ export default function OpenAccountPage() {
   const [debit_card, setDebitCard]=useState("");
   const [net_banking, setNet_banking]=useState("");
   
+  const [success, setSuccess] = useState(false)
+
+  const navigate = useNavigate()
+
+  // Model Handler
+  const [open, setOpen] = useState(true);
+  const handleClose = () => {
+    setOpen(false)
+    navigate('/')
+  };
+
 
   const onCustomerIdChange = (event) => {
     setCustomerId(event.target.value);
@@ -62,37 +101,131 @@ export default function OpenAccountPage() {
   };
 
   return (
-    <center>
-    <div>
-      <form onSubmit={onSubmitForm}>
-      <h2>Open a savings account</h2>
-      <label>Customer ID</label>
-      <input type="text" value={customerId} required onChange={onCustomerIdChange}/><br/>
-      <h3>Occupation details</h3>
-      <label>Occupation type</label>
-      <select value={occupationType} required onChange={onOccupationTypeChange}>
-        <option value="salary">Salaried professional</option>
-        <option value="business">Business</option>
-       </select><br/>
-       <label>Source of income</label>
-       <input type="text" value={sourceOfIncome} required onChange={onSourceOfIncomeChange}/><br/>
-       <label>Gross Annual income</label>
-        <input type="number" value={grossSalary} required onChange={onGrossSalaryChange}/><br/>
-        <label>Do you need debit card?</label>
-        {/* <input type="checkbox" onChange={onDebitChange} key={debit} name={debit} checked="false"/> */}
-        <select value={debit_card} required onChange={onDebitChange}>
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
-       </select><br/>
-       <label>Do you need Net banking?</label>
-        {/* <input type="checkbox" onChange={onDebitChange} key={debit} name={debit} checked="false"/> */}
-        <select value={net_banking} required onChange={onNetBankingChange}>
-        <option value="Yes">Yes</option>
-        <option value="No">No</option>
-       </select><br/>
-      <button type="submit">Submit</button>
-      </form>
-    </div>
-    </center>
+    <>
+      {!success ?
+        <>
+          <ThemeProvider theme={defaultTheme}>
+            <Container component="main" maxWidth="sm">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Open a account
+                </Typography>
+                <Box component="form" onSubmit={onSubmitForm} noValidate sx={{ mt: 1 }}>
+                  <Grid container columnSpacing={1.5}>
+                    <Grid item xs={6} >
+                      <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="customerId"
+                          label="Customer ID"
+                          name="customerId"
+                          autoComplete="customerId"
+                          onChange={onCustomerIdChange}
+                          autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={6} >
+                      <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="occupationType"
+                          label="Occupation type"
+                          name="occupationType"
+                          autoComplete="occupationType"
+                          onChange={onOccupationTypeChange}
+                          autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={6} >
+                      <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="sourceOfIncome"
+                          label="Source of income"
+                          name="sourceOfIncome"
+                          autoComplete="sourceOfIncome"
+                          onChange={onSourceOfIncomeChange}
+                          autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={6} >
+                      <TextField
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="annualIncome"
+                          label="Annual income"
+                          name="annualIncome"
+                          autoComplete="annualIncome"
+                          onChange={onGrossSalaryChange}
+                          autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={6} >
+                        <FormControlLabel
+                          label="Do you need a debit card?"
+                          control={<Checkbox checked={debit_card} onChange={onDebitChange}/>}
+                        />
+                    </Grid>
+                    <Grid item xs={6} >
+                        <FormControlLabel
+                          label="Do you need Net banking?"
+                          control={<Checkbox checked={net_banking} onChange={onNetBankingChange}/>}
+                        />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Open Account
+                  </Button>
+                </Box>
+              </Box>
+            </Container>
+          </ThemeProvider>
+        </>
+        :
+        <>
+          <div>
+            <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              open={open}
+              onClose={handleClose}
+              closeAfterTransition
+              slots={{ backdrop: Backdrop }}
+              slotProps={{
+                backdrop: {
+                  timeout: 500,
+                },
+              }}
+            >
+              <Fade in={open}>
+                <Box sx={style}>
+                  <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                    Account has been successfully created.
+                  </Typography>
+                </Box>
+              </Fade>
+            </Modal>
+          </div>
+        </>}
+    </>
   )
 }
