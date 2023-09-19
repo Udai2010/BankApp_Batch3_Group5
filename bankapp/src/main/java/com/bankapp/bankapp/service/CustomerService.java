241ade6c086651543eccf852b829157642d7819b
Kris.Dsz <kris.dsouza2001@gmail.com>
2023-09-19 07:52:34 +0530

    Added Change Password BE

--- bankapp/src/main/java/com/bankapp/bankapp/service/CustomerService.java
+++ bankapp/src/main/java/com/bankapp/bankapp/service/CustomerService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bankapp.bankapp.model.Customer;
 import com.bankapp.bankapp.model.Login;
@@ -43,5 +44,17 @@ public class CustomerService {
 
 	public Customer getCustomer(Long customer_id){
 		return customerRepo.findById(customer_id).get();
+	}
+	@Transactional
+	public String changePassword(Login l) {
+		String result = "Unsuccessful Password Update";
+		customerRepo.changePassword(l.getPassword(),l.getUserID());
+		
+		if(customerRepo.findById(l.getUserID()).get().getPassword().equals(l.getPassword()))
+		{		
+			result="Password updated successfully";
+				return result;
+		}
+		return result;
 	}
 }
