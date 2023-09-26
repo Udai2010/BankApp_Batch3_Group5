4496d1ad6749b008e12ddccb5335897097e49f4f
Udai Srinivasa Prasad K S <udai.prasad.5@gmail.com>
2023-09-26 10:35:45 +0530

    Perfect code

--- BankApp_Frontend/src/pages/OpenAccountPage.js
+++ BankApp_Frontend/src/pages/OpenAccountPage.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 import Avatar from "@mui/material/Avatar";
@@ -13,8 +14,6 @@ import { Checkbox, FormControlLabel, Grid } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
-import { Select } from "@mui/material";
-import { MenuItem } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const style = {
@@ -33,30 +32,27 @@ const defaultTheme = createTheme();
 
 export default function OpenAccountPage() {
   const token = localStorage.getItem("token");
-  const baseURL = "http://localhost:3000/createAccount/";
 
+  const baseURL = "http://localhost:3000/createAccount/";
   const [customerId, setCustomerId] = useState("");
   const [occupationType, setOccupationType] = useState("");
   const [sourceOfIncome, setSourceOfIncome] = useState("");
   const [grossSalary, setGrossSalary] = useState("");
   const [debit_card, setDebitCard] = useState("");
   const [net_banking, setNet_banking] = useState("");
-  const [account_type, setAccountType] = useState("");
-  const [branch, setBranch] = useState("");
+
   const [success, setSuccess] = useState(false);
-  const [message, setMessage] = useState("");
+
   const navigate = useNavigate();
 
   useEffect(() => {
     setCustomerId(window.sessionStorage.getItem("customer_id"));
   }, [customerId]);
-
   // Model Handler
   const [open, setOpen] = useState(true);
   const handleClose = () => {
     setOpen(false);
-    setSuccess(false);
-    navigate("/account");
+    navigate("/");
   };
 
   const onCustomerIdChange = (event) => {
@@ -83,42 +79,34 @@ export default function OpenAccountPage() {
     setNet_banking(event.target.value);
   };
 
-  const onBranchChange = (event) => {
-    setBranch(event.target.value);
-  };
-
-  const onAccountTypeChange = (event) => {
-    setAccountType(event.target.value);
-    console.log(account_type);
-  };
-
   const onSubmitForm = (event) => {
     event.preventDefault();
     const authToken = `Bearer ${token}`;
     const axiosInstance = axios.create({
-      baseURL: "http://localhost:3000/createAccount/", // Replace with your API URL
+      baseURL: "http://localhost:3000", // Replace with your API URL
       headers: {
         Authorization: authToken,
         "Content-Type": "application/json", // You can include other headers if needed
       },
     });
+
     axiosInstance
       .post(baseURL + customerId, {
+        occupation_type: occupationType,
+        income_source: sourceOfIncome,
+        annual_income: grossSalary,
         debit_card: debit_card,
-        branch: branch,
-        account_type: account_type,
+        branch: "BLR",
+        account_type: "Savings",
         net_banking: net_banking,
         status: "active",
         ifsc: "BLR1234A",
         balance: 5000,
-        occupation_type: occupationType,
-        income_source: sourceOfIncome,
-        annual_income: grossSalary,
       })
       .then((response) => {
-        setMessage(response.data);
-        setSuccess(true);
+        alert(response.data);
         console.log(response);
+        navigate('/account')
       })
       .catch((err) => {
         alert("error- " + err);
@@ -204,40 +192,6 @@ export default function OpenAccountPage() {
                         autoFocus
                       />
                     </Grid>
-
-                    <Grid item xs={6}>
-                      <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="branch"
-                        label="Branch"
-                        name="branch"
-                        autoComplete="branch"
-                        onChange={onBranchChange}
-                        autoFocus
-                      />
-                    </Grid>
-
-                    <Grid item xs={6}>
-                      <Select
-                        margin="normal"
-                        id="accountType"
-                        value={account_type}
-                        label="Account Type"
-                        name="accountType"
-                        onChange={onAccountTypeChange}
-                        required
-                        fullWidth
-                      >
-                        <MenuItem value={"Savings"}>Savings</MenuItem>
-                        <MenuItem value={"Fixed Deposit"}>
-                          Fixed Deposit
-                        </MenuItem>
-                        <MenuItem value={"Salary"}>Salary</MenuItem>
-                      </Select>
-                    </Grid>
-
                     <Grid item xs={6}>
                       <FormControlLabel
                         label="Do you need a debit card?"
@@ -293,7 +247,7 @@ export default function OpenAccountPage() {
               <Fade in={open}>
                 <Box sx={style}>
                   <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                    Account Created Successfully. {message}
+                    Account has been successfully created.
                   </Typography>
                 </Box>
               </Fade>
